45b46f90e1fcefb9dea3398babf9c5228b9519da
Kang Taehee <dev.thk28@gmail.com>
2022-09-27 17:40:02 +0900

    Feat: Firebase email 로그인 기능 구현

--- app/src/main/java/com/thk/instagram_clone/LoginActivity.kt
+++ app/src/main/java/com/thk/instagram_clone/LoginActivity.kt
@@ -1,11 +1,63 @@
 package com.thk.instagram_clone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import com.google.firebase.auth.FirebaseUser
+import com.thk.instagram_clone.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private val TAG = LoginActivity::class.simpleName
+    private lateinit var binding: ActivityLoginBinding
+
+    private val auth: FirebaseAuth by lazy { FirebaseAuth.getInstance() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            btnLoginEmail.setOnClickListener { signInAndSignUp() }
+        }
+    }
+
+    private fun signInAndSignUp() {
+        auth
+            .createUserWithEmailAndPassword(binding.etEmail.text.toString(), binding.etPassword.text.toString())
+            .addOnCompleteListener { task ->
+                when {
+                    task.isSuccessful -> moveToMainPage(task.result.user)
+                    task.exception is FirebaseAuthUserCollisionException -> signInWithEmail()
+                    task.exception is Exception -> {
+                        Log.w(TAG, "signInAndSignUp: Failure", task.exception)
+                        Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+    }
+
+    private fun signInWithEmail() {
+        auth
+            .signInWithEmailAndPassword(binding.etEmail.text.toString(), binding.etPassword.text.toString())
+            .addOnCompleteListener { task ->
+                when {
+                    task.isSuccessful -> moveToMainPage(task.result.user)
+                    else -> {
+                        Log.w(TAG, "signInAndSignUp: Failure", task.exception)
+                        Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+    }
+
+    private fun moveToMainPage(user: FirebaseUser?) {
+        if (user != null) {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
     }
 }
