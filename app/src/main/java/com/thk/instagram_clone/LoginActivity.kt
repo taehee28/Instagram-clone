f79d1500db3a207bcd4cb6f85ebe6477c960d943
Kang Taehee <dev.thk28@gmail.com>
2022-09-28 14:38:26 +0900

    Feat: 구글 계정 로그인 기능 구현
    * 구글 계정에 로그인 -> 계정 정보로 Firebase 로그인

--- app/src/main/java/com/thk/instagram_clone/LoginActivity.kt
+++ app/src/main/java/com/thk/instagram_clone/LoginActivity.kt
@@ -5,9 +5,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseAuthUserCollisionException
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import com.thk.instagram_clone.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
@@ -15,6 +23,23 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
 
     private val auth: FirebaseAuth by lazy { FirebaseAuth.getInstance() }
+    private val googleSignInClient: GoogleSignInClient by lazy {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        GoogleSignIn.getClient(this, gso)
+    }
+
+    private val googleLoginLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        val result = it.data?.let { intent -> Auth.GoogleSignInApi.getSignInResultFromIntent(intent) } ?: return@registerForActivityResult
+        if (result.isSuccess) {
+            signInWithGoogleAccount(result.signInAccount)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +48,7 @@ class LoginActivity : AppCompatActivity() {
 
         binding.apply {
             btnLoginEmail.setOnClickListener { signInAndSignUp() }
+            btnLoginGoogle.setOnClickListener { googleLogin() }
         }
     }
 
@@ -44,6 +70,25 @@ class LoginActivity : AppCompatActivity() {
     private fun signInWithEmail() {
         auth
             .signInWithEmailAndPassword(binding.etEmail.text.toString(), binding.etPassword.text.toString())
+            .addOnCompleteListener { task ->
+                when {
+                    task.isSuccessful -> moveToMainPage(task.result.user)
+                    else -> {
+                        Log.w(TAG, "signInAndSignUp: Failure", task.exception)
+                        Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+    }
+
+    private fun googleLogin() = googleLoginLauncher.launch(googleSignInClient.signInIntent)
+
+    private fun signInWithGoogleAccount(account: GoogleSignInAccount?) {
+        if (account == null) return
+
+        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+        auth
+            .signInWithCredential(credential)
             .addOnCompleteListener { task ->
                 when {
                     task.isSuccessful -> moveToMainPage(task.result.user)
