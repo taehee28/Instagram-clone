4d8fafefe4678505c0676ed1075686920a66ff37
Kang Taehee <dev.thk28@gmail.com>
2022-09-30 17:06:16 +0900

    Feat: 좋아요 버튼 기능 구현
    * 강의 내용과 다르게 구현한 부분
      - 기본 RecyclerView.Adapter 대신 ListAdapter 사용.
      - 업로드한 글의 uid를 다른 리스트로 저장하지 않고 ContentDto에
    프로퍼티 추가하여 사용(인덱스 안맞을 수 있는 문제 예방).
      - 좋아요 중복 방지를 위해 좋아요 누른 사람의 uid를 저장하는 map을
    사용하지 않는 대신에 liked 프로퍼티 사용해서 좋아요 눌렀는지 저장.
        -> 다른 사람이 좋아요를 누르면 다른 사람한테도 바뀐 좋아요가
    반영되는 문제 있음. 수정 필요.
    
    * 수정 필요한 부분
      - 좋아요 상태 바뀔 때 마다 해당 게시물이 깜빡이는 현상

--- app/src/main/java/com/thk/instagram_clone/navigation/DetailViewFragment.kt
+++ app/src/main/java/com/thk/instagram_clone/navigation/DetailViewFragment.kt
@@ -1,6 +1,7 @@
 package com.thk.instagram_clone.navigation
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -33,7 +34,10 @@ class DetailViewFragment : Fragment() {
     ): View? {
         _binding = FragmentDetailViewBinding.inflate(inflater, container, false)
 
-        binding.rvDetailList.adapter = listAdapter
+        binding.rvDetailList.adapter = listAdapter.apply {
+            likeClickEvent = onLikeClicked
+            setHasStableIds(true)
+        }
 
         getItemListFromFirestore()
 
@@ -54,9 +58,11 @@ class DetailViewFragment : Fragment() {
             .orderBy("timestamp")
             .addSnapshotListener { value, error ->
                 kotlin.runCatching {
-                    value?.documents?.map { it.toObject(ContentDto::class.java) } ?:
-                        throw IllegalArgumentException("Failed to get list(null returned)")
+                    value?.documents?.map {
+                        it.toObject(ContentDto::class.java)?.copy(contentUid = it.id)
+                    } ?: throw IllegalArgumentException("Failed to get list(null returned)")
                 }.onSuccess { list ->
+                    Log.d(TAG, "getItemListFromFirestore: $list")
                     listAdapter.submitList(list)
                 }.onFailure { e ->
                     e.printStackTrace()
@@ -64,20 +70,58 @@ class DetailViewFragment : Fragment() {
                 }
             }
     }
+
+    private val onLikeClicked = { uid: String?, isSelected: Boolean ->
+        if (!uid.isNullOrBlank()) {
+            // uid로 저장된 doc(업로드한 글) 찾아와서
+            // doc을 ContentDto로 변경하고
+            // ContentDto를 수정해서
+            // 수정된 Dto를 바탕으로 doc이 수정될 수 있게
+            // transaction으로 set 해줌
+
+            Log.d(TAG, "onLikeClicked: uid = $uid")
+
+            val tsDoc = firestore.collection("images").document(uid)
+
+            firestore.runTransaction { transaction ->
+                val item = transaction.get(tsDoc).toObject(ContentDto::class.java) ?: return@runTransaction
+                transaction.set(
+                    tsDoc,
+                    item.copy(
+                        likeCount = if (isSelected) item.likeCount + 1 else item.likeCount - 1,
+                        liked = isSelected
+                    )
+                )
+            }
+        }
+    }
 }
 
 /**
  * RecyclerView Adapter class
  */
 class DetailListAdapter : ListAdapter<ContentDto, DetailListAdapter.DetailViewHolder>(DetailDiffUtil()) {
+    private val TAG = DetailListAdapter::class.simpleName
+
+    var likeClickEvent: ((String?, Boolean) -> Unit)? = null
 
     inner class DetailViewHolder(private val binding: ItemDetailViewBinding) : ViewHolder(binding.root) {
+        private var uid: String? = null
+
         init {
             // onClick 설정하기
-
+            binding.btnLike.setOnClickListener { view ->
+                Log.d(TAG, "before selected: ${view.isSelected}")
+                view.isSelected = !view.isSelected
+                Log.d(TAG, "after selected: ${view.isSelected}")
+                likeClickEvent?.invoke(uid, view.isSelected)
+            }
         }
 
         internal fun bind(item: ContentDto) {
+            uid = item.contentUid
+            Log.d(TAG, "bind: contentUid = $uid")
+
             binding.apply {
                 tvUserName.text = item.userId ?: "null"
 
@@ -93,6 +137,8 @@ class DetailListAdapter : ListAdapter<ContentDto, DetailListAdapter.DetailViewHo
                 tvDescription.text = item.description
 
                 tvLikeCount.text = item.likeCount.toString()
+
+                btnLike.isSelected = item.liked
             }
         }
     }
@@ -105,14 +151,18 @@ class DetailListAdapter : ListAdapter<ContentDto, DetailListAdapter.DetailViewHo
     override fun onBindViewHolder(holder: DetailViewHolder, position: Int) {
         holder.bind(getItem(position))
     }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
 }
 
 class DetailDiffUtil : DiffUtil.ItemCallback<ContentDto>() {
     override fun areItemsTheSame(oldItem: ContentDto, newItem: ContentDto): Boolean {
-        return oldItem == newItem
+        return (oldItem.uid == newItem.uid) and (oldItem.timestamp == newItem.timestamp)
     }
 
     override fun areContentsTheSame(oldItem: ContentDto, newItem: ContentDto): Boolean {
-        return oldItem == newItem
+        return (oldItem.likeCount == newItem.likeCount) or (oldItem.liked == newItem.liked)
     }
 }
