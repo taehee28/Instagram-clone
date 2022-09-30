f5f0545194e62287cb30555b25eabb1c4371ff75
Kang Taehee <dev.thk28@gmail.com>
2022-09-30 15:27:39 +0900

    Feat: 메인 리스트 화면 구현
    * Firestore에 저장되어있는 포스트를 가져와서 목록으로 표시
      - DetailViewFragment에 RecyclerView 추가
      - RecyclerView 구현에 필요한 ListAdapter와 DiffUtil 클래스 구현
    
    * 그 외 변경 사항
      - Glide 라이브러리 제대로 다시 추가
      - detail view xml layout margin/padding 조정
      - ContentDto의 프로퍼티들이 초기화 될 수 있도록 변경(역직렬화
    가능하도록)

--- app/src/main/java/com/thk/instagram_clone/model/ContentDto.kt
+++ app/src/main/java/com/thk/instagram_clone/model/ContentDto.kt
@@ -1,17 +1,17 @@
 package com.thk.instagram_clone.model
 
 data class ContentDto(
-    val description: String,
-    val imageUrl: String,
-    val uid: String?,
-    val userId: String?,
-    val timestamp: Long,
+    val description: String = "",
+    val imageUrl: String = "",
+    val uid: String? = "",
+    val userId: String? = "",
+    val timestamp: Long = 0,
     val favoriteCount: Int = 0
 ) {
     data class Comment(
-        val uid: String,
-        val userId: String,
-        val text: String,
-        val timestamp: Long
+        val uid: String = "",
+        val userId: String = "",
+        val text: String = "",
+        val timestamp: Long = 0
     )
 }
--- app/src/main/java/com/thk/instagram_clone/navigation/DetailViewFragment.kt
+++ app/src/main/java/com/thk/instagram_clone/navigation/DetailViewFragment.kt
@@ -5,13 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.google.firebase.firestore.FirebaseFirestore
+import com.thk.instagram_clone.GlideApp
 import com.thk.instagram_clone.R
 import com.thk.instagram_clone.databinding.FragmentDetailViewBinding
+import com.thk.instagram_clone.databinding.ItemDetailViewBinding
+import com.thk.instagram_clone.model.ContentDto
 
 class DetailViewFragment : Fragment() {
     private val TAG = DetailViewFragment::class.simpleName
     private var _binding: FragmentDetailViewBinding? = null
     private val binding get() = _binding!!
+
+    private val firestore: FirebaseFirestore by lazy { FirebaseFirestore.getInstance() }
+    private val listAdapter = DetailListAdapter()
 
     companion object {
         @JvmStatic
@@ -24,11 +34,86 @@ class DetailViewFragment : Fragment() {
     ): View? {
         _binding = FragmentDetailViewBinding.inflate(inflater, container, false)
 
+        binding.rvDetailList.adapter = listAdapter
+
+        getItemListFromFirestore()
+
         return binding.root
     }
 
     override fun onDestroyView() {
         _binding = null
         super.onDestroyView()
+    }
+
+    /**
+     * Firestore로부터 post 목록을 가져와 list dapter에 전달 
+     */
+    private fun getItemListFromFirestore() {
+        firestore
+            .collection("images")
+            .orderBy("timestamp")
+            .addSnapshotListener { value, error ->
+                kotlin.runCatching {
+                    value?.documents?.map { it.toObject(ContentDto::class.java) } ?:
+                        throw IllegalArgumentException("Failed to get list(null returned)")
+                }.onSuccess { list ->
+                    listAdapter.submitList(list)
+                }.onFailure { e ->
+                    e.printStackTrace()
+                    error?.printStackTrace()
+                }
+            }
+    }
+}
+
+/**
+ * RecyclerView Adapter class
+ */
+class DetailListAdapter : ListAdapter<ContentDto, DetailListAdapter.DetailViewHolder>(DetailDiffUtil()) {
+
+    inner class DetailViewHolder(private val binding: ItemDetailViewBinding) : ViewHolder(binding.root) {
+        init {
+            // onClick 설정하기
+
+        }
+
+        internal fun bind(item: ContentDto) {
+            binding.apply {
+                tvUserName.text = item.userId ?: "null"
+
+                // TODO: 추후 진짜 프로필 사진 받아오는 것으로 변경 예정
+                GlideApp.with(ivProfile)
+                    .load(item.imageUrl)
+                    .into(ivProfile)
+
+                GlideApp.with(ivPhoto)
+                    .load(item.imageUrl)
+                    .into(ivPhoto)
+
+                tvDescription.text = item.description
+
+                tvLikeCount.text = item.favoriteCount.toString()
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DetailViewHolder {
+        val binding = ItemDetailViewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return DetailViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: DetailViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+}
+
+class DetailDiffUtil : DiffUtil.ItemCallback<ContentDto>() {
+    override fun areItemsTheSame(oldItem: ContentDto, newItem: ContentDto): Boolean {
+        return oldItem == newItem
+    }
+
+    override fun areContentsTheSame(oldItem: ContentDto, newItem: ContentDto): Boolean {
+        return oldItem == newItem
     }
 }
