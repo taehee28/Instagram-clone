bce660df7e04841c57674bdf72c90f2453dd126a
Kang Taehee <dev.thk28@gmail.com>
2022-10-06 12:15:43 +0900

    Feat: 프로필 이미지 설정 기능 구현

--- app/src/main/java/com/thk/instagram_clone/navigation/AccountFragment.kt
+++ app/src/main/java/com/thk/instagram_clone/navigation/AccountFragment.kt
@@ -1,14 +1,19 @@
 package com.thk.instagram_clone.navigation
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import com.thk.instagram_clone.adapter.PostListAdapter
 import com.thk.instagram_clone.util.Firebase
 import com.thk.instagram_clone.databinding.FragmentAccountBinding
 import com.thk.instagram_clone.model.ContentDto
+import com.thk.instagram_clone.util.GlideApp
 
 class AccountFragment : Fragment() {
     private val TAG = AccountFragment::class.simpleName
@@ -18,6 +23,15 @@ class AccountFragment : Fragment() {
     private val uid: String? get() = Firebase.auth.currentUser?.uid
 
     private val listAdapter = PostListAdapter()
+
+    private val albumLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            // 사진을 선택했을 때
+            uploadProfileImage(result.data?.data)
+        }
+    }
 
     companion object {
         @JvmStatic
@@ -37,13 +51,39 @@ class AccountFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        getProfileImageFromFirestore()
         getPostListFromFirestore()
+
         binding.btnProfile.setOnClickListener { Firebase.auth.signOut() }
+        binding.ivProfile.setOnClickListener {
+            val imagePickerIntent = Intent(Intent.ACTION_PICK).apply { type = "image/*" }
+            albumLauncher.launch(imagePickerIntent)
+        }
     }
 
     override fun onDestroyView() {
         _binding = null
         super.onDestroyView()
+    }
+
+    private fun getProfileImageFromFirestore() = kotlin.runCatching {
+        Firebase.firestore
+            .collection("profileImages")
+            .document(uid!!)
+            .addSnapshotListener { value, error ->
+                kotlin.runCatching {
+                    value?.data?.get("image") ?: throw IllegalArgumentException("Failed to get profile image(null returned)")
+                }.onSuccess {
+                    GlideApp.with(binding.ivProfile)
+                        .load(it)
+                        .circleCrop()
+                        .into(binding.ivProfile)
+                }.onFailure {
+                    it.printStackTrace()
+                    error?.printStackTrace()
+                }
+            }
     }
 
     private fun getPostListFromFirestore() {
@@ -62,5 +102,17 @@ class AccountFragment : Fragment() {
                     error?.printStackTrace()
                 }
             }
+    }
+
+    private fun uploadProfileImage(imageUri: Uri?) = kotlin.runCatching {
+        Firebase.storage.reference.child("userProfileImages").child(uid!!).also { ref ->
+            ref.putFile(imageUri!!)
+                .continueWithTask { ref.downloadUrl }
+                .addOnSuccessListener { uri ->
+                    val map = mapOf("image" to uri.toString())
+                    Firebase.firestore.collection("profileImages").document(uid!!).set(map)
+                }
+        }
+
     }
 }
