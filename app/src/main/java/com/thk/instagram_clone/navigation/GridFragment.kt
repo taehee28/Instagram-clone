9353100f71eabe748ca4bf46c2009b0d991ba0de
Kang Taehee <dev.thk28@gmail.com>
2022-10-18 11:28:24 +0900

    GridFragment에 DataBinding 적용

--- app/src/main/java/com/thk/instagram_clone/navigation/GridFragment.kt
+++ app/src/main/java/com/thk/instagram_clone/navigation/GridFragment.kt
@@ -5,24 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
+import com.thk.instagram_clone.R
 import com.thk.instagram_clone.adapter.PostListAdapter
 import com.thk.instagram_clone.databinding.FragmentGridBinding
 import com.thk.instagram_clone.viewmodel.DetailViewViewModel
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class GridFragment : Fragment() {
-    private val TAG = GridFragment::class.simpleName
     private var _binding: FragmentGridBinding? = null
     private val binding get() = _binding!!
 
     private val viewModel: DetailViewViewModel by viewModels()
-    private val listAdapter = PostListAdapter()
 
     companion object {
         @JvmStatic
@@ -33,21 +35,17 @@ class GridFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentGridBinding.inflate(inflater, container, false)
+        _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_grid, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.rvPostList.adapter = listAdapter
 
-        lifecycleScope.launch {
-            viewModel.itemsFlow
-                .flowWithLifecycle(lifecycle, Lifecycle.State.STARTED)
-                .distinctUntilChanged()
-                .collectLatest {
-                    listAdapter.submitList(it)
-                }
+        binding.apply {
+            lifecycleOwner = viewLifecycleOwner
+            vm = viewModel
+            adapter = PostListAdapter()
         }
     }
 
