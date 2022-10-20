bd50c89c6785cad09f39d08de9a648836af8316d
Kang Taehee <dev.thk28@gmail.com>
2022-10-20 14:00:38 +0900

    Rename: Util.kt를 LoadingDialog.kt로 이름 변경

--- app/src/main/java/com/thk/instagram_clone/util/LoadingDialog.kt
+++ app/src/main/java/com/thk/instagram_clone/util/LoadingDialog.kt
@@ -4,7 +4,6 @@ import android.app.Dialog
 import android.content.Context
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
-import android.util.Log
 import com.thk.instagram_clone.R
 
 /**
